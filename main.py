25fb3637acbe5be4c29122407e274871c65e2cf3
Vasco Lameiras <vasco.lameiras@oceanfone.com>
2017-08-23 12:51:30 +0100

    refactoring

--- main.py
+++ main.py
@@ -37,26 +37,14 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
 
-def on_open(ws):
-    def run(*args):
-        #for i in range(3):
-        #    time.sleep(1)
-        #    ws.send("Hello %d" % i)
-        time.sleep(99999999)
-        ws.close()
-        print("thread terminating...")
-    thread.start_new_thread(run, ())
-
-
 if __name__ == "__main__":
     poe_trade_url_ini = raw_input("Please enter your poe.trade URL: ")
     poe_trade_url_split = poe_trade_url_ini.split("search/", 1)[1]
-    
     poe_trade_url = poe_trade_url_split.split("/live",1)[0]
+
     websocket.enableTrace(False)
     ws = websocket.WebSocketApp("ws://live.poe.trade/" + poe_trade_url,
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
-    ws.on_open = on_open
     ws.run_forever()
